080eae4affb09dbcbc26c1686c3292e59ad861e2
Rushikesh <105331213+Rushikesh-Patmase@users.noreply.github.com>
2024-03-28 02:43:01 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ const Navbar = () => {
     return ( 
         <nav className="navbar">
             <div className='shadow-2xl flex flex-row bg-black justify-center space-x-10'>
-                <span className='text-orange-600 font-bold text-xl p-4 pr-72'>MyFolio.</span>
+                <span className='text-orange-600 font-bold text-xl p-4 w-1/4'>MyFolio.</span>
                 <Link to='/' className='rounded-none text-white p-4 transform hover:bg-cyan-900 transition duration-500 hover:text-orange-500'>About Me</Link>
                 <Link to='/education' className='rounded-none text-white p-4 transform hover:bg-cyan-900 transition duration-500 hover:text-orange-500'>Education</Link>
                 <Link to='/projects' className='rounded-none text-white p-4 transform hover:bg-cyan-900 transition duration-500 hover:text-orange-500'>Projects</Link>
