0028ce8404205d9b0c9d644fdf7ddc114e86ec55
Rushikesh <105331213+Rushikesh-Patmase@users.noreply.github.com>
2024-03-29 03:10:24 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -19,9 +19,9 @@ const Navbar = () => {
                 <li><Link to='/contact' className='rounded-none text-white p-4 transform hover:bg-cyan-900 transition duration-500 hover:text-orange-500'>Contact</Link></li>
             </ul>
             <div onClick={ handleNav }>
-                { !nav ? <AiOutlineClose size={30} className='text-white'/> : <AiOutlineMenu size={30} className='text-white md:hidden' /> }
+                { !nav ? <AiOutlineClose size={30} className='text-white md:hidden'/> : <AiOutlineMenu size={30} className='text-white md:hidden' /> }
             </div>
-            <div className={ !nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-grey-900 bg-emerald-900 ease-in-out duration-500' : 'fixed left-[-100%]'}>
+            <div className={ !nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-grey-900 bg-emerald-900 ease-in-out duration-500 md:hidden' : 'fixed left-[-100%]'}>
                 <h1 className='text-orange-600 font-bold text-2xl m-4 p-4 w-full'>MyFolio.</h1>
                 <ul className='uppercase p-2'>
                     <li className='p-4 border-b border-gray-500'><Link to='/' className='rounded-none text-white p-4 hover:text-orange-500'>About</Link></li>
