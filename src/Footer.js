7cc4e28507525de9da537a77d3fd1e458844c376
Rushikesh <105331213+Rushikesh-Patmase@users.noreply.github.com>
2024-03-29 03:00:44 +0530

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (  
         <div className='bg-black flex flex-row'>
             <div className='flex flex-col mx-40'>
-                <span className='text-orange-600 font-bold text-2xl mx-8 my-5'>MyFolio.</span>
+                <h1 className='text-orange-600 font-bold text-2xl mx-8 my-5'>MyFolio.</h1>
                 <div className='flex flex-row mx-5'>
                     <a href='https://www.instagram.com/_rushikeshh___'><FaInstagram className='w-10 h-5 text-white' /></a> 
                     <a href='https://www.linkedin.com/rushikesh-mukund-patmase-895b9023b'><FaLinkedin className='w-10 h-5 text-white' /></a> 
