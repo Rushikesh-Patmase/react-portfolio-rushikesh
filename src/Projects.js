f4b09feec50e3a1f639ecebd1e9164d8aeec7c10
Rushikesh <105331213+Rushikesh-Patmase@users.noreply.github.com>
2024-04-02 01:07:22 +0530

    Update Projects.js

--- src/Projects.js
+++ src/Projects.js
@@ -4,7 +4,7 @@ const Projects = () => {
             <div className='bg-white bg-opacity-30 md:mx-48 m-5 p-5 rounded-lg shadow-2xl flex flex-col'>
                 <p className='font-bold text-center text-2xl pb-5 '>Projects.</p>
                 <div className='flex flex-col justify-center space-y-5'>
-                    <div className='bg-emerald-400 bg-opacity-20 m-5 md:mx-28 p-5 transform hover:bg-emerald-600 transition duration-500 rounded-lg text-center'>
+                    <div className='bg-emerald-400 bg-opacity-20 m-5 md:mx-28 p-5 hover:bg-emerald-600 transition duration-500 rounded-lg text-center'>
                         <span className='bg-blue-800 bg-opacity-20 text-white font-sans rounded-full my-2 px-4 py-1'>September, 2023 - present</span>
                         <p className='font-bold mt-2 text-xl'>Guardian Air-Sensor Driven Airbag Inflation for SIDS Prevention.</p>
                         <p className=' text-md'>This project focuses on preventing the infant deaths caused due to suffocation while sleeping in prone position.
@@ -14,16 +14,16 @@ const Projects = () => {
                                 of their little ones during sleep.</p>
                         <p className=' text-md'><b>Tech Stack:</b> Python and Arduino</p>
                     </div>
-                    <div className='bg-emerald-400 bg-opacity-20 m-5 md:mx-28 p-5 transform hover:bg-emerald-600 transition duration-500 rounded-lg text-center'>
+                    <div className='bg-emerald-400 bg-opacity-20 m-5 md:mx-28 p-5 hover:bg-emerald-600 transition duration-500 rounded-lg text-center'>
                         <span className='bg-blue-800 bg-opacity-20 text-white rounded-full font-sans my-2 px-4 py-1'>April, 2023 - May, 2023</span>
                         <p className='font-bold mt-2 text-xl'>Tic Tac Toe Game.</p>
                         <p className=' text-md'>A fun TIC-TAC-TOE Game with a simple yet attractive web interface using technologies HTML, CSS and JavaScript
                                 which also displays the current player's turn and also displays the winner. This project provides the freedom from
                                 boredom by letting the players apply strategic thinking skills which improves the cognitive function of their brain.</p>
                         <p className=' text-md'><b>Tech Stack:</b> HTML, CSS and Javascript</p><br/>
-                        <a href="https://rushikesh-patmase.github.io/" className="bg-emerald-100 p-3 transform hover:bg-emerald-400 transition duration-500">View Project Demo</a>
+                        <a href="https://rushikesh-patmase.github.io/" className="bg-emerald-100 p-3 hover:bg-emerald-400 transition duration-500">View Project Demo</a>
                     </div>
-                    <div className='bg-emerald-400 bg-opacity-20 m-5 md:mx-28 p-5 transform hover:bg-emerald-600 transition duration-500 rounded-lg text-center'>
+                    <div className='bg-emerald-400 bg-opacity-20 m-5 md:mx-28 p-5 hover:bg-emerald-600 transition duration-500 rounded-lg text-center'>
                         <span className='bg-blue-800 bg-opacity-20 text-white rounded-full font-sans my-2 px-4 py-1'>July, 2022 - December, 2022</span>
                         <p className='font-bold mt-2 text-xl'>Student Management System Portal.</p>
                         <p className=' text-md'>The project mainly focuses on the management of academics of the students of an institution through a website
@@ -32,7 +32,7 @@ const Projects = () => {
                                 those results from the interface.</p>
                         <p className=' text-md'><b>Tech Stack:</b> HTML, CSS, Javascript, MySQL and PHP.</p>
                     </div>
-                    <div className='bg-emerald-400 bg-opacity-20 m-5 md:mx-28 p-5 transform hover:bg-emerald-600 transition duration-500 rounded-lg text-center'>
+                    <div className='bg-emerald-400 bg-opacity-20 m-5 md:mx-28 p-5 hover:bg-emerald-600 transition duration-500 rounded-lg text-center'>
                         <span className='bg-blue-800 bg-opacity-20 text-white rounded-full font-sans my-2 px-4 py-1'>March, 2022 - May, 2022</span>
                         <p className='font-bold mt-2 text-xl'>Youtuber Database Management.</p>
                         <p className=' text-md'>Sample application of SQL standards using Youtubers Data where the concepts of normalization, grouping of
